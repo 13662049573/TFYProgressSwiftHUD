495101806d6b9df46598ffd849837c76eab99fc3
田风有 <420144542@qq.com>
2022-06-16 13:46:26 +0800

    持续更新中
    持续更新中

--- TFYProgressSwiftHUD/TFYProgressSwiftHUD/TFYProgressSwiftUilt.swift
+++ TFYProgressSwiftHUD/TFYProgressSwiftHUD/TFYProgressSwiftUilt.swift
@@ -8,10 +8,9 @@
 import Foundation
 import UIKit
 
-
 ///动画1 设置
 func animationSystemActivityIndicator(_ view:UIView,color:UIColor) {
-    let spinner = UIActivityIndicatorView(style: .whiteLarge)
+    let spinner = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.large)
     spinner.frame = view.bounds
     spinner.color = color
     spinner.hidesWhenStopped = true
